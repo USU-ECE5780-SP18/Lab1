61057f5f05ccdce91514c0168296657086044b60
John Call <johnkc1992@gmail.com>
2018-02-16 13:14:09 -0700

    Implement outline of part 6

---
+++ src/part6.adb
@@ -0,0 +1,40 @@
+with Ada.Text_IO;
+use  Ada.Text_IO;
+
+
+procedure Part6 is
+	task type buffer is
+	end buffer;
+	
+	task type producer is
+	end producer;
+	
+	task type consumer is
+	end consumer;
+	
+	
+	task body buffer is
+		full: Boolean := false;
+		
+	begin
+		
+	end buffer;
+	
+	task body producer is
+		
+	begin
+		
+	end producer;
+	
+	task body consumer is
+		
+	begin
+		
+	end consumer;
+	
+	t_buff : buffer;
+	t_prod : producer;
+	t_cons : consumer;
+begin
+	null;
+end Part6;
